a681e6caf31d0889703d4e2b72fba7d4e7c7482b
Balgopal Mishra <kanhamishra1a@gmail.com>
2025-07-16 23:21:45 +0530

    Secured project by ignoring .env and database files

--- myproject/settings.py
+++ myproject/settings.py
@@ -22,10 +22,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-)f4vm#9#9#yjz3$%h2fi2=9f%osz2j_i%b^y+2j^p618uu&ohf'
+from decouple import config
 
-# SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+SECRET_KEY = config('SECRET_KEY')
+DEBUG = config('DEBUG', cast=bool)
 
 ALLOWED_HOSTS = []
 
